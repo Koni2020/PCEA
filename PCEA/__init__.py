754ebe1fcb36c219e108507571a7a125409ceddc
Koni2020 <1526203879@qq.com>
2024-12-30 18:21:13 +0800

    Rename test function name

--- PCEA/__init__.py
+++ PCEA/__init__.py
@@ -1,4 +1,4 @@
 from .cea import CEA
 from .cea_core import find_consecutive, find_compound_event
 from .test import PCEA_test
-__all__ = [CEA, find_consecutive, find_compound_event, pyCEA_test]
+__all__ = [CEA, find_consecutive, find_compound_event, PCEA_test]
